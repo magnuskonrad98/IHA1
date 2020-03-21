5c72deadbf15dc6eb05940540b44e263afbc1a56
Your Name <magnuskonrad@gmail.com>
2020-03-21 17:32:51 +0000

    added test for a single integer

--- sample.py/add.py
+++ sample.py/add.py
@@ -1,3 +1,5 @@
 def add(numbers):
     if numbers == "":
         return 0
+    else:
+        return int(numbers)
--- sample.py/add_tests.py
+++ sample.py/add_tests.py
@@ -5,6 +5,9 @@ class MyFirstTests(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(""), 0)
 
+        self.assertEqual(add("1"), 1)
+        self.assertEqual(add("4"), 4)
+
 
 if __name__ == '__main__':
     unittest.main()

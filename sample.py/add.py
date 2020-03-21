d2bf3edf3812176064c0c6b816255d4212816404
Your Name <magnuskonrad@gmail.com>
2020-03-21 17:49:56 +0000

    added part of function for two numbers

--- sample.py/add.py
+++ sample.py/add.py
@@ -3,3 +3,6 @@ def add(numbers):
         return 0
     elif "," not in numbers:
         return int(numbers)
+    else:
+        num_a, num_b = numbers.split(",")
+        return int(num_a) + int(num_b)
--- sample.py/add_tests.py
+++ sample.py/add_tests.py
@@ -8,6 +8,10 @@ class MyFirstTests(unittest.TestCase):
         self.assertEqual(add("1"), 1)
         self.assertEqual(add("4"), 4)
 
+        self.assertEqual(add("1,2"), 3)
+        self.assertEqual(add("5,7"), 12)
+        self.assertEqual(add("4,6"), 10)
+
 
 if __name__ == '__main__':
     unittest.main()

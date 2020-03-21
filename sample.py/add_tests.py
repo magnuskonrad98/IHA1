7b92bc600fd126020236e8d8ea69c8685a324746
Your Name <magnuskonrad@gmail.com>
2020-03-21 17:09:57 +0000

    first test added

---
+++ sample.py/add_tests.py
@@ -0,0 +1,10 @@
+import unittest
+from add import add
+
+class MyFirstTests(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(""), "0")
+
+
+if __name__ == '__main__':
+    unittest.main()
